542720edc16cdcbb3e628de36974aa4937f56bf9
Natsu-Efrain <natsusalamander8888@gmail.com>
2021-05-21 17:37:30 -0500

    Prueba

--- SistemaPosgrado/Sistema/views.py
+++ SistemaPosgrado/Sistema/views.py
@@ -2,7 +2,8 @@ from django.http.request import HttpHeaders, HttpRequest
 from django.shortcuts import render
 from django.http.response import HttpResponse
 # Create your views here.
-#Metodo de vizualización de Login
+# Prueba de commit
+#Metodo #1 de vizualización de Login
 def Login (request):
     return render(request,"Login.html")
 #Metodo de vizualización de Login
