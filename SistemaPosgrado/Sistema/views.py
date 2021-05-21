a19740f3c98cbfedec1e5eca5b6f20a38b99fc93
IAN PONCE <juan.perezlol@hotmail.com>
2021-05-21 18:16:54 -0500

    Version1 prueba 2

--- SistemaPosgrado/Sistema/views.py
+++ SistemaPosgrado/Sistema/views.py
@@ -2,7 +2,7 @@ from django.http.request import HttpHeaders, HttpRequest
 from django.shortcuts import render
 from django.http.response import HttpResponse
 # Create your views here.
-# Prueba de commit Ian Mira
+# Prueba de commit Efrain 
 #Metodo #1 de vizualización de Login
 def Login (request):
     return render(request,"Login.html")
